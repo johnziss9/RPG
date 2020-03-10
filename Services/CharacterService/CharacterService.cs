373d86484e6c4c0c6a729e2ee5049a3a7c88b5bf
Zissimou <ZissimouJ@b2b.regn.net>
2020-03-10 15:38:28 +0000

    Created a new class in Models called ServiceResponse and referenced it in the interface and class of CharacterService's methods.

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -13,20 +13,29 @@ namespace dotnet_rpg.Services.CharacterService
             new Character { Id = 1, Name = "Sam" }
         };
 
-        public async Task<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
+            ServiceResponse<List<Character>> serviceReponse = new ServiceResponse<List<Character>>();
+
             characters.Add(newCharacter);
-            return characters;
+            serviceReponse.Data = characters;
+            return serviceReponse;
         }
 
-        public async Task<List<Character>> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-            return characters;
+            ServiceResponse<List<Character>> serviceReponse = new ServiceResponse<List<Character>>();
+
+            serviceReponse.Data = characters;
+            return serviceReponse;
         }
 
-        public async Task<Character> GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-            return characters.FirstOrDefault(c => c.Id == id);
+            ServiceResponse<Character> serviceReponse = new ServiceResponse<Character>();
+
+            serviceReponse.Data = characters.FirstOrDefault(c => c.Id == id);
+            return serviceReponse;
         }
     }
 }
